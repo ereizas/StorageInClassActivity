82357813de85f1ee4d6b6599b9bad87ca7f517b8
Eric Reizas <tuo17224@temple.edu>
2024-04-11 12:37:36 -0400

    Saved comic

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -14,6 +14,12 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
+import java.util.prefs.Preferences
 
 // TODO (1: Fix any bugs)
 // TODO (2: Add function saveComic(...) to save and load comic info automatically when app starts)
@@ -26,8 +32,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+    private val cachedComicJSONFile = "comic"
+    private lateinit var file : File
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //TODO check if comic was loaded previously and display it
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -38,17 +47,21 @@ class MainActivity : AppCompatActivity() {
         numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
-
+        file = File(filesDir,cachedComicJSONFile)
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
         }
 
     }
 
+
     private fun downloadComic (comicId: String) {
+        //TODO make the stored comic persistent
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
-            JsonObjectRequest(url, {showComic(it)}, {
+            JsonObjectRequest(url, {
+                showComic(it)
+                saveComic(it)}, {
             })
         )
     }
@@ -57,6 +70,20 @@ class MainActivity : AppCompatActivity() {
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
+        saveComic(comicObject)
+    }
+
+    private fun saveComic(comic: JSONObject)
+    {
+        try{
+            val otptStream = FileOutputStream(file)
+            otptStream.write(comic.toString().toByteArray())
+            otptStream.close()
+        }
+        catch(e:Exception){
+            e.printStackTrace()
+            super.onStop()
+        }
     }
 
 
